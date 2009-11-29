d287d871dc9b364df456cc86317817f7fd672991
Brian Rosner <brosner@gmail.com>
2009-11-29 12:10:05 -0700

    bumped version for a Pinax release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-openid',
-    version='0.2.0',
+    version='0.2.1',
     description='OpenID tools for Django',
     author='Simon Willison',
     author_email='simon@simonwillison.net',
