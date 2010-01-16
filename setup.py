f86a7fea99c5bc79d665b79f55a473287ce5df1c
Brian Rosner <brosner@gmail.com>
2010-01-16 12:52:26 -0700

    use development builds now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-openid',
-    version='0.2.2',
+    version='0.2.3.dev1',
     description='OpenID tools for Django',
     author='Simon Willison',
     author_email='simon@simonwillison.net',
