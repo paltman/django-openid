552144067615fe487d6518325f1e7913e6dd6442
Patrick Altman <paltman@gmail.com>
2014-04-03 16:05:41 -0500

    0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-openid',
-    version='0.3a1',
+    version='0.4',
     description='OpenID tools for Django',
     author='Simon Willison',
     author_email='simon@simonwillison.net',
